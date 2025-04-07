bf9b1022d1f2c7bd23a0b7797ac24209541a8256
Bobisawesome07 <100872835+Bobisawesome07@users.noreply.github.com>
2025-04-07 17:57:46 -0500

    google style guide

--- src/main/java/com/hightech/discordbot/Bot.kt
+++ src/main/java/com/hightech/discordbot/Bot.kt
@@ -4,9 +4,7 @@ import io.github.cdimascio.dotenv.Dotenv
 import net.dv8tion.jda.api.EmbedBuilder
 import net.dv8tion.jda.api.JDABuilder
 import net.dv8tion.jda.api.Permission
-import net.dv8tion.jda.api.entities.Message
 import net.dv8tion.jda.api.entities.Role
-import net.dv8tion.jda.api.entities.channel.concrete.PrivateChannel
 import net.dv8tion.jda.api.events.interaction.command.SlashCommandInteractionEvent
 import net.dv8tion.jda.api.hooks.ListenerAdapter
 import net.dv8tion.jda.api.interactions.commands.DefaultMemberPermissions
@@ -14,26 +12,23 @@ import net.dv8tion.jda.api.interactions.commands.OptionType
 import net.dv8tion.jda.api.interactions.commands.build.Commands
 import net.dv8tion.jda.api.interactions.commands.build.OptionData
 import net.dv8tion.jda.api.requests.GatewayIntent
-import net.dv8tion.jda.api.requests.RestAction
 import net.dv8tion.jda.api.utils.MemberCachePolicy
 import nl.vv32.rcon.Rcon
 import java.awt.Color
 import java.io.IOException
 import java.sql.DriverManager
 import java.sql.SQLException
-import java.util.function.Function
 
 class Bot : ListenerAdapter() {
     override fun onSlashCommandInteraction(event: SlashCommandInteractionEvent) {
         if (event.guild == null) return
+
         when (event.name) {
             "santa" -> {
                 println("santa command called")
-                val role =
-                    event.getOption("role")!!.asRole // the "user" option is required, so it doesn't need a null-check here
+                val role = event.getOption("role")!!.asRole
                 santa(event, role)
             }
-
             "online" -> {
                 println("online command called")
                 try {
@@ -42,7 +37,6 @@ class Bot : ListenerAdapter() {
                     throw RuntimeException(e)
                 }
             }
-
             "todo" -> {
                 println("todo command called")
                 try {
@@ -56,73 +50,84 @@ class Bot : ListenerAdapter() {
 
     @Throws(SQLException::class)
     fun todo(event: SlashCommandInteractionEvent) {
-        val Purple = Color(59, 0, 64)
-        val Dark_Purple = Color(80, 1, 46)
+        val purple = Color(59, 0, 64)
+        val darkPurple = Color(80, 1, 46)
         val hook = event.hook
-        val sTodo = ArrayList<String>()
-        val cTodo = ArrayList<String>()
-        val sList = EmbedBuilder()
-        val cList = EmbedBuilder()
-        sList.setTitle("Survival Todo List")
-        cList.setTitle("Creative Todo List")
-        sList.setColor(Purple)
-        cList.setColor(Dark_Purple)
-        sList.setTimestamp(event.timeCreated)
-        cList.setTimestamp(event.timeCreated)
+        val survivalTodos = ArrayList<String>()
+        val creativeTodos = ArrayList<String>()
+        val survivalList = EmbedBuilder()
+        val creativeList = EmbedBuilder()
+
+        survivalList.setTitle("Survival Todo List")
+        creativeList.setTitle("Creative Todo List")
+        survivalList.setColor(purple)
+        creativeList.setColor(darkPurple)
+        survivalList.setTimestamp(event.timeCreated)
+        creativeList.setTimestamp(event.timeCreated)
+
         val dotenv = Dotenv.configure().load()
         val url = dotenv["DATABASE_URL"]
-        val con = DriverManager.getConnection(url)
-        val st = con.createStatement()
-        val st2 = con.createStatement()
-        val rt = st.executeQuery("SELECT type FROM myschema.\"todos\"")
-        val tt = st2.executeQuery("SELECT title FROM myschema.\"todos\"")
+        val connection = DriverManager.getConnection(url)
+        val statement = connection.createStatement()
+        val statement2 = connection.createStatement()
+        val typeResult = statement.executeQuery("SELECT type FROM myschema.\"todos\"")
+        val titleResult = statement2.executeQuery("SELECT title FROM myschema.\"todos\"")
+
         event.deferReply(true).queue()
-        while (rt.next()) {
-            if (rt.getString(1) == "survival") {
-                tt.next()
-                sTodo.add(tt.getString("title"))
-            } else if (rt.getString(1) == "creative") {
-                tt.next()
-                cTodo.add(tt.getString("title"))
-            } else {
-                System.err.println(rt.getString(1) + " is not a real server type")
+
+        while (typeResult.next()) {
+            when (typeResult.getString(1)) {
+                "survival" -> {
+                    titleResult.next()
+                    survivalTodos.add(titleResult.getString("title"))
+                }
+                "creative" -> {
+                    titleResult.next()
+                    creativeTodos.add(titleResult.getString("title"))
+                }
+                else -> {
+                    System.err.println("${typeResult.getString(1)} is not a real server type")
+                }
             }
-            println(rt.getString(1))
-        }
-        rt.close()
-        st.close()
-        val sListDescription = StringBuilder()
-        val cListDescription = StringBuilder()
-
-        for (todo in sTodo) {
-            sListDescription.append("• ").append(todo).append("\n")
+            println(typeResult.getString(1))
         }
 
-        for (todo in cTodo) {
-            cListDescription.append("• ").append(todo).append("\n")
+        typeResult.close()
+        statement.close()
+
+        val survivalDescription = StringBuilder()
+        val creativeDescription = StringBuilder()
+
+        for (todo in survivalTodos) {
+            survivalDescription.append("• ").append(todo).append("\n")
         }
-        sList.setDescription(sListDescription.toString())
-        cList.setDescription(cListDescription.toString())
-        val i = sList.build()
-        val c = cList.build()
-        hook.sendMessageEmbeds(i, c).queue()
+
+        for (todo in creativeTodos) {
+            creativeDescription.append("• ").append(todo).append("\n")
+        }
+
+        survivalList.setDescription(survivalDescription.toString())
+        creativeList.setDescription(creativeDescription.toString())
+
+        val survivalEmbed = survivalList.build()
+        val creativeEmbed = creativeList.build()
+        hook.sendMessageEmbeds(survivalEmbed, creativeEmbed).queue()
     }
 
     @Throws(IOException::class)
     fun online(event: SlashCommandInteractionEvent) {
         val dotenv = Dotenv.configure().load()
         event.deferReply(false).queue()
-        val o = event.guild
-        val g = o!!.getRoleById(dotenv["MEMBER_ROLE_ID"].toLong())
+        val guild = event.guild
+        val memberRole = guild!!.getRoleById(dotenv["MEMBER_ROLE_ID"].toLong())
         val hook = event.hook
         val rcon = Rcon.open(dotenv["SERVER_IP"], dotenv["PORT"].toInt())
-        run {
-            if (rcon.authenticate(dotenv["RCON_PASSWORD"]) && event.member!!.roles.contains(g)) {
-                hook.sendMessage(rcon.sendCommand("list").toString()).queue()
-                rcon.close()
-            } else {
-                println("Failed to authenticate")
-            }
+
+        if (rcon.authenticate(dotenv["RCON_PASSWORD"]) && event.member!!.roles.contains(memberRole)) {
+            hook.sendMessage(rcon.sendCommand("list").toString()).queue()
+            rcon.close()
+        } else {
+            println("Failed to authenticate")
         }
     }
 
@@ -130,38 +135,44 @@ class Bot : ListenerAdapter() {
         val dotenv = Dotenv.configure().load()
         event.deferReply(true).queue()
         val hook = event.hook
+
         if (!event.member!!.hasPermission(Permission.ADMINISTRATOR)) {
             println("santa command not sent by admin")
             hook.sendMessage("You don't have the required permissions to run that command.").queue()
             return
         }
-        val memList = ArrayList<String>()
+
+        val membersList = ArrayList<String>()
         val guild = checkNotNull(event.guild)
         val admin = guild.getMemberById(dotenv["ADMIN_ID"].toLong())
-        val mems = guild.getMembersWithRoles(role)
-        for (j in mems) {
-            memList.add(j.effectiveName)
+        val members = guild.getMembersWithRoles(role)
+
+        for (member in members) {
+            membersList.add(member.effectiveName)
         }
+
         println("Gifter, Receiver")
-        for (i in mems) {
-            var j = Math.random() * memList.size
-            while (memList[j.toInt()] === i.effectiveName) {
-                j = Math.random() * memList.size
+
+        for (member in members) {
+            var randomIndex = Math.random() * membersList.size
+            while (membersList[randomIndex.toInt()] == member.effectiveName) {
+                randomIndex = Math.random() * membersList.size
             }
-            val mess = "Make your thing for " + memList[j.toInt()] + (" dm admin with any questions!")
-            val foradmin = i.effectiveName + ", " + memList[j.toInt()]
-            i.user.openPrivateChannel()
-                .flatMap { channel: PrivateChannel -> channel.sendMessage(mess) }
+
+            val message = "Make your thing for ${membersList[randomIndex.toInt()]} dm admin with any questions!"
+            val adminMessage = "${member.effectiveName}, ${membersList[randomIndex.toInt()]}"
+
+            member.user.openPrivateChannel()
+                .flatMap { channel -> channel.sendMessage(message) }
                 .queue()
+
             admin!!.user.openPrivateChannel()
-                .flatMap((Function<PrivateChannel, RestAction<Message>> { channel: PrivateChannel ->
-                    channel.sendMessage(
-                        foradmin
-                    )
-                }))
+                .flatMap { channel -> channel.sendMessage(adminMessage) }
                 .queue()
-            memList.removeAt(j.toInt())
+
+            membersList.removeAt(randomIndex.toInt())
         }
+
         hook.sendMessage("Success!").queue()
     }
 
@@ -182,9 +193,10 @@ class Bot : ListenerAdapter() {
                 .setMemberCachePolicy(MemberCachePolicy.ALL)
                 .build().awaitReady()
 
-            val guildy = jda.getGuildById(dotenv["GUILD_ID"].toLong())
-            val member = guildy!!.getRoleById(dotenv["MEMBER_ROLE_ID"].toLong())
-            guildy.loadMembers()
+            val guild = jda.getGuildById(dotenv["GUILD_ID"].toLong())
+            val member = guild!!.getRoleById(dotenv["MEMBER_ROLE_ID"].toLong())
+            guild.loadMembers()
+
             val commands = jda.updateCommands()
             commands.addCommands(
                 Commands.slash("santa", "Generates Secret Santa Partners and dms them")

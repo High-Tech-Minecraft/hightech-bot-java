9e9245df51f6a27ad0ed562495abfa01b038787f
caleb.koopman <bobisawesome07@outlook.com>
2025-05-06 06:13:54 -0500

    Update dependencies and Gradle configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,14 +1,12 @@
 plugins {
     application
-    id("com.gradleup.shadow") version "8.3.1"
+    id("com.github.johnrengelman.shadow") version "8.1.1"
     kotlin("jvm")
 }
 
 application.mainClass = "com.hightech.discordbot.Bot" //
 group = "org.hightech"
 version = "1.0"
-
-val jdaVersion = "5.2.1" //
 
 repositories {
     mavenCentral()
@@ -20,19 +18,12 @@ repositories {
 }
 
 dependencies {
-    implementation ("io.github.cdimascio:dotenv-java:2.3.0")
+    implementation ("io.github.cdimascio:dotenv-java:3.0.0")
     implementation ("nl.vv32.rcon:rcon:1.2.0")
-    implementation("org.postgresql:postgresql:42.7.2")
-    implementation ("com.mattmalec:Pterodactyl4J:2.BETA_142")
-    testImplementation(platform("org.junit:junit-bom:5.10.0"))
-    testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("net.dv8tion:JDA:5.2.1")
-    implementation(kotlin("stdlib-jdk8"))
+    implementation("org.postgresql:postgresql:42.7.5")
+    implementation("net.dv8tion:JDA:5.5.1")
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
 kotlin {
     jvmToolchain(21)
 }
